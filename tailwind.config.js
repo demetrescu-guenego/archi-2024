a50745910db9edd1fd1dfd0575790328a89a26e5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-21 10:28:10 +0200

    dev: added tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./.vitepress/theme/**/*.{vue,js,ts,jsx,tsx,html,md}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
