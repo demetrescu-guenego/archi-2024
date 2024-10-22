3a4cea11fa98879d683ff1bb2b4ba4885bf025d0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-22 10:01:05 +0200

    dev: added pages

--- .vitepress/theme/layout/BodyLayout.vue
+++ .vitepress/theme/layout/BodyLayout.vue
@@ -2,6 +2,8 @@
 import { useData, Content } from "vitepress";
 import HomePage from "../models/HomePage.vue";
 import RealisationPage from "../models/RealisationPage.vue";
+import ClientPage from "../models/ClientPage.vue";
+import ContactPage from "../models/ContactPage.vue";
 
 // https://vitepress.dev/reference/runtime-api#usedata
 const { site, frontmatter } = useData();
@@ -10,6 +12,8 @@ const { site, frontmatter } = useData();
 <template>
   <HomePage v-if="frontmatter.layout === 'home'" />
   <RealisationPage v-if="frontmatter.layout === 'realisations'" />
+  <ClientPage v-if="frontmatter.layout === 'clients'" />
+  <ContactPage v-if="frontmatter.layout === 'contact'" />
   <div v-else>
     <a href="/">Home</a>
     <Content />
---
+++ .vitepress/theme/models/ClientPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex-grow flex flex-col">
+    <h1>Clients</h1>
+  </main>
+</template>
