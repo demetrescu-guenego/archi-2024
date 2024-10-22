189c3bace2017e83e8dc5a296115638d86cf661a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-22 09:55:00 +0200

    dev: added realisation page

--- .vitepress/theme/layout/BodyLayout.vue
+++ .vitepress/theme/layout/BodyLayout.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useData, Content } from "vitepress";
 import HomePage from "../models/HomePage.vue";
+import RealisationPage from "../models/RealisationPage.vue";
 
 // https://vitepress.dev/reference/runtime-api#usedata
 const { site, frontmatter } = useData();
 </script>
 
 <template>
-  <HomePage v-if="frontmatter.home" />
+  <HomePage v-if="frontmatter.layout === 'home'" />
+  <RealisationPage v-if="frontmatter.layout === 'realisations'" />
   <div v-else>
     <a href="/">Home</a>
     <Content />
---
+++ .vitepress/theme/models/RealisationPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex-grow flex flex-col">
+    <h1>Réalisations</h1>
+  </main>
+</template>
