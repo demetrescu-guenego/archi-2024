81d69469e3a6f2daaa33bad8ea05efd094b19203
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-22 11:41:15 +0200

    dev: added text-center

--- .vitepress/theme/layout/FooterLayout.vue
+++ .vitepress/theme/layout/FooterLayout.vue
@@ -6,7 +6,7 @@ import ThisYear from "./../widgets/ThisYear.vue";
   <footer
     class="p-4 flex items-center justify-center bg-neutral-400 text-white"
   >
-    <span>
+    <span class="text-center">
       Copyright © 2003-<ThisYear /> - Cabinet d'Architecture Demetrescu -
       Guénégo. Tout droit réservés. Site réalisé par
       <a class="underline" href="https://jlg-consulting.com" target="_blank"
