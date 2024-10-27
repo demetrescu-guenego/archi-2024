510afc68be877582083811130a0b0ee9d3e3673f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-27 15:35:09 +0100

    dev: window only after onMounted

--- .vitepress/theme/models/ViewerPage.vue
+++ .vitepress/theme/models/ViewerPage.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import ZoomableImage from "../widgets/ZoomableImage.vue";
 
 // TODO: desktop only: ll the photo zoomable with mouse scroll and pan with mousemove
 
 const src = ref("/home/ferrieres-landscape-big.jpg");
 
-if ("window" in globalThis) {
-  const params = Object.fromEntries(
-    new URLSearchParams(globalThis.window.location.search),
-  );
-  if (params.src) {
-    src.value = params.src;
+onMounted(() => {
+  if ("window" in globalThis) {
+    const params = Object.fromEntries(
+      new URLSearchParams(globalThis.window.location.search),
+    );
+    if (params.src) {
+      src.value = params.src;
+    }
   }
-}
+});
 </script>
 
 <template>
