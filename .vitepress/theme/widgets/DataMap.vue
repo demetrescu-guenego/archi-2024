51ae9fdc410f643af10ce0dd6a6ab7bd48fea6a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-11-14 17:04:26 +0100

    dev: green for icons

--- .vitepress/theme/widgets/DataMap.vue
+++ .vitepress/theme/widgets/DataMap.vue
@@ -25,12 +25,21 @@ onMounted(() => {
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  L.marker([48.78648245905831, 2.712753660065945])
-    .addTo(map)
-    .bindPopup("A pretty CSS popup.<br> Easily customizable.");
+  const greenIcon = new L.Icon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
 
   for (const localisation of props.localisations) {
-    L.marker([localisation.gps.latitude, localisation.gps.longitude])
+    L.marker([localisation.gps.latitude, localisation.gps.longitude], {
+      icon: greenIcon,
+    })
       .addTo(map)
       .bindPopup(
         `<a href="${localisation.url}" style="color: black;">${localisation.label} (${localisation.zipcode})</a>`,
