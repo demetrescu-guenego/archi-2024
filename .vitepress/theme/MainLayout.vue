c39a35bd50efadb57804dc871274ca31a2947028
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-29 15:12:56 +0200

    feat: redirect to search with ctrl+k

--- .vitepress/theme/MainLayout.vue
+++ .vitepress/theme/MainLayout.vue
@@ -3,6 +3,29 @@ import HeaderLayout from "./layout/HeaderLayout.vue";
 import FooterLayout from "./layout/FooterLayout.vue";
 import BodyLayout from "./layout/BodyLayout.vue";
 import RegisterSW from "./widgets/RegisterSW.vue";
+import { onMounted, onUnmounted } from "vue";
+import { useRouter } from "vitepress";
+
+const router = useRouter();
+
+const handleKeydown = (event: KeyboardEvent) => {
+  if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "k") {
+    event.preventDefault();
+    router.go("/search");
+  }
+};
+
+onMounted(() => {
+  if ("window" in globalThis) {
+    window.addEventListener("keydown", handleKeydown);
+  }
+});
+
+onUnmounted(() => {
+  if ("window" in globalThis) {
+    window.removeEventListener("keydown", handleKeydown);
+  }
+});
 </script>
 
 <template>
